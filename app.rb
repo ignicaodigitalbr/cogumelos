6ec1dbc35904183ac0d3f642c1fe4e65cc49b292
José Roniérison <joseronierison@gmail.com>
2016-11-24 17:58:38 -0300

    Passo 2

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
+require 'pp'
+
 keys = ['edible','cap-shape','cap-surface','cap-color','bruises?','odor','gill-attachment','gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring','stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type','veil-color','ring-number','ring-type','spore-print-color','population','habitat']
 training_set = []
 
 File.readlines('expanded').each do |line|
-  attributes = line.split(',')
+  attributes = line.gsub(/\n/, '').split(',')
 
   if attributes.length < 23
     next
@@ -16,3 +18,11 @@ File.readlines('expanded').each do |line|
 
   training_set.push(mushroom)
 end
+
+puts "Quantidade de cogumelos: #{training_set.length}"
+flat_mushrooms = training_set.select do |feature|
+  feature['cap-shape'] == 'FLAT'
+end
+
+pp flat_mushrooms.length
+
