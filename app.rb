4512e6c40ac3593c2b83d927e19a1a898714ddb4
Henrique H. da Silva <henrique@dryworks.com.br>
2016-11-24 19:39:10 -0200

    Passo 5

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'pp'
 
 keys = ['edible','cap-shape','cap-surface','cap-color','bruises?','odor','gill-attachment','gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring','stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type','veil-color','ring-number','ring-type','spore-print-color','population','habitat']
+
 training_set = []
 results = {}
 
@@ -24,7 +25,19 @@ end
 #   feature['edible'] == 'POISONOUS'
 # end
 
-cap_shape_types = training_set.uniq{|x| x['cap-shape']}.map{|i| i['cap-shape']}
-cap_shape_count = training_set.count{|f| f['cap-shape'] == "FLAT"}
+keys.each do |key|
+  next if key == 'edible'
+
+  types = training_set.uniq{|x| x[key]}.map{|i| i[key]}
+
+  pp key
+  pp types
+end
+
+
+
+cap_shape_flat_count = training_set.count{|f| f['cap-shape'] == "FLAT" }.to_f
+
+cap_shape_count = training_set.count{|f| f['cap-shape'] == "FLAT" && f['edible'] == 'POISONOUS'}.to_f / cap_shape_flat_count * 100
 
 pp cap_shape_count
